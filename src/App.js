343be7789813e0475cbf9dee16155680c04cd9d2
NickWaldock <nickwaldock@me.com>
2023-05-27 14:04:33 +0000

    Install react bootstrap, add CDN link to index.html, crossorigin prop to head link, and bootstrap button in App.js to test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Hello World!
-      </h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
