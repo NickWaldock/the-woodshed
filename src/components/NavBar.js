3a519719245c72b7090181a6690d104a4a705055
NickWaldock <nickwaldock@me.com>
2023-06-02 15:06:29 +0000

    Add additional placeholder links for all pages to NavBar.js, adapt NavBar css for font size

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,12 +13,15 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>    
-              Home</Nav.Link>
-            <Nav.Link>
-              Sign In</Nav.Link>
-            <Nav.Link>
-              Sign Up</Nav.Link>
+            <Nav.Link>HOME</Nav.Link>
+            <Nav.Link>SIGN IN</Nav.Link>
+            <Nav.Link>SIGN UP</Nav.Link>
+            <Nav.Link>REGISTER</Nav.Link>
+            <Nav.Link>FEED</Nav.Link>
+            <Nav.Link>LIKED</Nav.Link>
+            <Nav.Link>FOLLOWING</Nav.Link>
+            <Nav.Link>ADD POST</Nav.Link>
+            <Nav.Link>PROFILE</Nav.Link>
           </Nav> 
         </Navbar.Collapse>
       </Container>
