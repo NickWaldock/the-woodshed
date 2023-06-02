e7a34c64f091c65deae90e2e0b282f1c3343975f
NickWaldock <nickwaldock@me.com>
2023-06-02 14:05:50 +0000

    Add google fonts link to index.html head, create styles folder and Navbar.module.css file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,13 +12,12 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
+            <Nav.Link>    
+              Home</Nav.Link>
             <Nav.Link>
-                <i className="fas fa-home"></i>
-                Home</Nav.Link>
+              Sign In</Nav.Link>
             <Nav.Link>
-            <i className="fas fa-sign-in-alt"></i>Sign In</Nav.Link>
-            <Nav.Link>
-            <i className="fas fa-user-plus"></i>Sign Up</Nav.Link>
+              Sign Up</Nav.Link>
           </Nav> 
         </Navbar.Collapse>
       </Container>
