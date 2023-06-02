a71f3b200304d12dd5905a0759ac0e66ef9ed818
NickWaldock <nickwaldock@me.com>
2023-06-02 15:02:52 +0000

    Install React Router, Add router to index.js, add routes with placeholder for home page and signin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
-      <NavBar>
-        
-      </NavBar>
+      <NavBar />
+        <Container className={styles.Main}>
+          <Switch>
+            <Route exact path="/" render={() => <h1>Home Page</h1>} />
+            <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          </Switch>
+        </Container>
     </div>
   );
 }
