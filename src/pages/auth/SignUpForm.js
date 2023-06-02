2a31249d0aa177895947809126fffba90ca19c66
NickWaldock <nickwaldock@me.com>
2023-06-02 17:02:32 +0000

    Add bootstrap form to SignInForm.js, style in SignInUpForm.module.css

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-import logo from '../../assets/main-logo.png'
+import logo from "../../assets/main-logo.png";
 
 import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
 
@@ -13,10 +13,24 @@ const SignUpForm = () => {
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>SIGN UP</h1>
+          <h1 className={styles.Header}>Sign up</h1>
+          <Form className="mt-4">
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control type="text" placeholder="Username" name="username" className={styles.FormText} />
+              <Form.Text className={`${styles.FormText} text-muted`}>
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-            {/* add your form here */}
-
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" className={styles.FormText} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signin">
@@ -28,10 +42,7 @@ const SignUpForm = () => {
         md={6}
         className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
       >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={logo}
-        />
+        <Image className={`${appStyles.FillerImage}`} src={logo} />
       </Col>
     </Row>
   );
