2787151c8ff4f4c279ae787d269993f272b09634
NickWaldock <nickwaldock@me.com>
2023-06-02 16:47:52 +0000

    Create SignInUpForm.module.css, and SignUpForm.js, style sign in page, create SignUpForm component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
 import {Route, Switch} from 'react-router-dom';
 import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           <Switch>
             <Route exact path="/" render={() => <h1>Home Page</h1>} />
             <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
-            <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+            <Route exact path="/signup" render={() => <SignUpForm />} />
             <Route exact path="/register" render={() => <h1>Register</h1>} />
             <Route exact path="/add-post" render={() => <h1>Add post</h1>} />
             <Route exact path="/feed" render={() => <h1>Feed</h1>} />
---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+import logo from '../../assets/main-logo.png'
+
+import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+
+const SignUpForm = () => {
+  return (
+    <Row className={styles.Row}>
+      <Col className="my-auto py-2 p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>SIGN UP</h1>
+
+            {/* add your form here */}
+
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signin">
+            Already have an account? <span>Sign in</span>
+          </Link>
+        </Container>
+      </Col>
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+      >
+        <Image
+          className={`${appStyles.FillerImage}`}
+          src={logo}
+        />
+      </Col>
+    </Row>
+  );
+};
+
+export default SignUpForm;
