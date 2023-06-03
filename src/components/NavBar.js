a166fde3e03f632cc63429331df964501fe20bbd
NickWaldock <nickwaldock@me.com>
2023-06-03 14:36:59 +0000

    Add logic to NavBar.js to auto close the burger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo-1.png";
 import styles from "../styles/NavBar.module.css";
@@ -23,6 +23,22 @@ const NavBar = () => {
       console.log(err);
     }
   };
+
+  // Burger Menu Logic
+  const [expanded, setExpanded] = useState(false);
+  const ref = useRef(null)
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)){
+        setExpanded(false)
+      }
+    }
+    document.addEventListener('mouseup', handleClickOutside)
+    return () => {
+      document.removeEventListener('mouseup', handleClickOutside)
+    }
+  }, [ref]);
+
 
   // Conditional NavBar links
   const addPostLink = (
@@ -102,7 +118,12 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar expand="md" fixed="top" className={styles.NavBar}>
+    <Navbar
+      expanded={expanded}
+      expand="md"
+      fixed="top"
+      className={styles.NavBar}
+    >
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -110,7 +131,11 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostLink}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle 
+          ref={ref}
+          onClick={()=> setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav" 
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink exact to="/" className={styles.NavLink}>
