64906237ec43d88377c13cdfb158ef53e1754edb
NickWaldock <nickwaldock@me.com>
2023-06-05 16:36:10 +0000

    Add to Post.js to display user avatar and updated_at info

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,8 +1,40 @@
 import React from 'react'
 import styles from '../../styles/Post.module.css'
+import { useCurrentUser } from '../../contexts/CurrentUserContexts';
+import { Card, Media } from 'react-bootstrap';
+import { Avatar } from '../../components/Avatar';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 export const Post = (props) => {
+    const {
+        id,
+        owner,
+        profile_id,
+        profile_image,
+        comments_count,
+        likes_count,
+        like_id,
+        content,
+        image,
+        updated_at
+    } = props;
+
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner
+
   return (
-    <div>Post baby yea</div>
+    <Card className={styles.Post}>
+			<Card.Body>
+				<Media className="align-items-center justify-content-between">
+					<Link to={`/profiles/${profile_id}`}>
+						<Avatar src={profile_image} height={55} />
+						{owner}
+					</Link>
+					<div className='d-flex align-items-center'>
+						<span>{updated_at}</span>
+					</div>
+				</Media>
+			</Card.Body>
+    </Card>
   )
 }
