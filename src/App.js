f9328caa92ed0d7223e6153f5258a6df721b8cb4
NickWaldock <nickwaldock@me.com>
2023-06-05 16:18:00 +0000

    Create PostPage.js, write PostPage component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
+import PostPage from "./pages/posts/PostPage";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route exact path="/feed" render={() => <h1>Feed</h1>} />
           <Route exact path="/liked" render={() => <h1>Liked</h1>} />
           <Route exact path="/following" render={() => <h1>Following</h1>} />
