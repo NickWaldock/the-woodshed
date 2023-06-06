1bb4da008889abbaa4bcb8c3de38e2a714f54dde
NickWaldock <nickwaldock@me.com>
2023-06-06 12:26:05 +0000

    Added useState hooks and useEffect hooks in PoostsPage.js for the loading spinner adn api fetch request

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
@@ -7,9 +7,28 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
-function PostsPage() {
-  
+function PostsPage({message, filter=""}) {
+  const [posts, setPosts] = useState({results: []});
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const {pathname} = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+        try {
+            const {data} = await axiosReq.get(`/posts/?${filter}`)
+            setPosts(data)
+            setHasLoaded(true)
+        } catch(err){
+            console.log(err)
+        }
+    }
+    setHasLoaded(false);
+    fetchPosts();
+}, [filter, pathname]);
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
