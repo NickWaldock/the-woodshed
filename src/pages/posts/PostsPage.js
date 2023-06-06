71cf25d50143ee6fc0c08e6bd889d9f80170d57a
NickWaldock <nickwaldock@me.com>
2023-06-06 14:18:23 +0000

    Add search bar in to PostsPage.js component, add timer to make api requests after 1sec

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -13,15 +13,17 @@ import { axiosReq } from "../../api/axiosDefaults";
 import { Post } from "./Post";
 import Asset from "../../components/Asset";
 
-function PostsPage({message, filter=""}) {
+function PostsPage({message, filter=''}) {
   const [posts, setPosts] = useState({results: []});
   const [hasLoaded, setHasLoaded] = useState(false);
   const {pathname} = useLocation();
 
+  const [query, setQuery] = useState('');
+
   useEffect(() => {
     const fetchPosts = async () => {
         try {
-            const {data} = await axiosReq.get(`/posts/?${filter}`)
+            const {data} = await axiosReq.get(`/posts/?${filter}search=${query}`)
             setPosts(data)
             setHasLoaded(true)
         } catch(err){
@@ -29,13 +31,33 @@ function PostsPage({message, filter=""}) {
         }
     }
     setHasLoaded(false);
-    fetchPosts();
-}, [filter, pathname]);
+    const timer = setTimeout(() => {
+      fetchPosts();
+    }, 1000)
+    return () => {
+      clearTimeout(timer)
+    }
+}, [filter, query, pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
+
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form 
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control 
+            value={query}
+            type="text" 
+            className="mr-sm-2" 
+            placeholder="Search..." 
+            onChange={(event) => setQuery(event.target.value)}
+          />
+        </Form>
+
         {hasLoaded ? (
             <>
             {posts.results.length ? (
