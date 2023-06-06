4978d27431b01b997a823da14f071afbcc5f7b49
NickWaldock <nickwaldock@me.com>
2023-06-06 11:53:24 +0000

    Adjust PostsPage route  props in App.js for a 'no results found' message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,13 @@ function App() {
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <PostsPage />} />
+          <Route 
+            exact  
+            path="/" 
+            render={() => (
+              <PostsPage message="No results found. Adjust the search keyword." />
+              )}
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
