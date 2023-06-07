ec45f5134fa5ba19bb296c04bdb9c61782a610bc
NickWaldock <nickwaldock@me.com>
2023-06-07 14:00:35 +0000

    Add popperConfig prop to Dropdown menu in MoreDropdown.js

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -1,41 +1,44 @@
 import React from "react";
-import styles from "../styles/MoreDropdown.module.css"
+import styles from "../styles/MoreDropdown.module.css";
 import { Dropdown } from "react-bootstrap";
 
 // The forwardRef is important!!
 // Dropdown needs access to the DOM node in order to position the Menu
 const ThreeDots = React.forwardRef(({ onClick }, ref) => (
-    <i
-      className="fa-solid fa-screwdriver-wrench fa-xl"
-      ref={ref}
-      onClick={(e) => {
-        e.preventDefault();
-        onClick(e);
-      }}
-    />
-  ));
+  <i
+    className="fa-solid fa-screwdriver-wrench fa-xl"
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  />
+));
 
 // Render the dropdown menu for editing and deleting a post
-  export const MoreDropdown = ({handleEdit, handleDelete}) => {
-		return (
-			<Dropdown className="ml-auto" drop="left">
+export const MoreDropdown = ({ handleEdit, handleDelete }) => {
+  return (
+    <Dropdown className="ml-auto" drop="left">
       <Dropdown.Toggle as={ThreeDots} />
-      <Dropdown.Menu className="text-center">
-        <Dropdown.Item 
-					className={styles.DropdownItem}
-					onClick={handleEdit}
-					aria-label="edit"
-				>
-				<i class="fa-solid fa-wrench fa-lg"></i>
-				</Dropdown.Item>
-        <Dropdown.Item 
-					className={styles.DropdownItem}
-					onClick={handleDelete}
-					aria-label="delete"
-				>
-					<i class="fa-solid fa-trash-can fa-lg"></i>
-				</Dropdown.Item>
+      <Dropdown.Menu
+        className="text-center"
+        popperConfig={{ strategy: "fixed" }}
+      >
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={handleEdit}
+          aria-label="edit"
+        >
+          <i class="fa-solid fa-wrench fa-lg"></i>
+        </Dropdown.Item>
+        <Dropdown.Item
+          className={styles.DropdownItem}
+          onClick={handleDelete}
+          aria-label="delete"
+        >
+          <i class="fa-solid fa-trash-can fa-lg"></i>
+        </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
-		); 
-	};
+  );
+};
