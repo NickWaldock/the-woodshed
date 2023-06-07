b7dc516a1ba9efcbeb702ab7d63d2c662608ab3c
NickWaldock <nickwaldock@me.com>
2023-06-07 16:02:32 +0000

    Add logic to map through and display comments in PostPage.js

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -23,11 +23,12 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-				const [{data: post}] = await Promise.all([
+				const [{data: post}, {data: comments}] = await Promise.all([
 					axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`)
 				])
-				setPost({results: [post]})
-				console.log(post)
+				setPost({results: [post]});
+				setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -52,6 +53,17 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
+          {comments.results.length ? (
+            comments.results.map(comment => (
+              <p key={comment.id}>
+            {comment.owner}: {comment.content}
+          </p>
+            ))
+          ) : currentUser ? (
+            <span>No comments so far, be the first!</span>
+          ) : (
+            <span>No comments...</span>
+          )}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
