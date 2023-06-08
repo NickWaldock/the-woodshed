96e4893b48142095e4ed93e1eb539c3ef6d74c23
NickWaldock <nickwaldock@me.com>
2023-06-08 11:16:25 +0000

    Add useEffect funtion to PopularProfiles.js component for the api request to get follower_count

--- src/profiles/PopularProfiles.js
+++ src/profiles/PopularProfiles.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import appStyles from '../../App.module.css'
+import React, { useEffect, useState } from 'react'
+import appStyles from '../../src/App.module.css'
 import { Container } from 'react-bootstrap'
+import { axiosReq } from '../api/axiosDefaults';
 
 const PopularProfiles = () => {
+	const [profileData, setProfileData] = useState({
+    pageProfile: {results: [] },
+		popularProfiles: { results: [] },
+	});
+	const {popularProfiles} = profileData;
+	const currentUser = useCurrentUser();
+
+	useEffect(() => {
+		const handleMount = async () => {
+			try {
+				const {data} = await axiosReq.get(
+					'/profiles/?ordering=-followers_count'
+				);
+				setProfileData(prevState => ({
+					...prevState,
+					popularProfiles: data,
+				}));
+			} catch(err){
+				console.log(err)
+			}
+		};
+		handleMount();
+	}, [currentUser]);
+
   return (
     <Container className={appStyles.Content}>
-        <p>most followed profiles</p>
+        <p>Most followed profiles</p>
     </Container>
   )
 }
