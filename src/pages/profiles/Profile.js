77a11901db660e866913b5b8e0a90c2c2590737a
NickWaldock <nickwaldock@me.com>
2023-06-08 12:32:56 +0000

    Add props to Profile.js and links with user avatars to component

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import styles from '../../styles/Profile.module.css';
 import btnStyles from '../../styles/Button.module.css';
+import { useCurrentUser } from '../../contexts/CurrentUserContexts';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import Avatar from '../../components/Avatar';
 
-const Profile = () => {
+const Profile = (props) => {
+    const {profile, mobile, imageSize=55} = props;
+    const {id, following_id, image, owner} = profile;
+
+    // Get which user is logged in to hide their profile
+    const currentUser = useCurrentUser();
+    const is_owner = currentUser?.username === owner;
+
   return (
-    <div>Profile</div>
+    <div
+      className={`my-3 d-flex align-items-center ${mobile && 'flex-column'}`}
+    >
+      <div>
+				<Link className='align-self-center' to={`/profiles/${id}`}>
+					<Avatar src={image} height={imageSize} />
+				</Link>
+				<div className={`mx-2 ${styles.WordBreak}`}>
+					<strong>{owner}</strong>
+				</div>
+			</div> 
+    </div>
   )
 }
 
