33f04406870ee5e4a641b0f9d68cdd1cd129395f
NickWaldock <nickwaldock@me.com>
2023-06-08 13:20:33 +0000

    Create src/contexts/ProfileDataContext.js and refactor code from PopularProfiles.js to create a profile data context component for all pages

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,32 +1,55 @@
-import React from 'react';
-import styles from '../../styles/Profile.module.css';
-import btnStyles from '../../styles/Button.module.css';
-import { useCurrentUser } from '../../contexts/CurrentUserContexts';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import Avatar from '../../components/Avatar';
+import React from "react";
+import styles from "../../styles/Profile.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContexts";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Avatar from "../../components/Avatar";
+import { Button } from "react-bootstrap";
 
 const Profile = (props) => {
-    const {profile, mobile, imageSize=55} = props;
-    const {id, following_id, image, owner} = profile;
+  const { profile, mobile, imageSize = 55 } = props;
+  const { id, following_id, image, owner } = profile;
 
-    // Get which user is logged in to hide their profile
-    const currentUser = useCurrentUser();
-    const is_owner = currentUser?.username === owner;
+  // Get which user is logged in to hide their profile
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
   return (
     <div
-      className={`my-3 d-flex align-items-center ${mobile && 'flex-column'}`}
+      className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
     >
       <div>
-				<Link className='align-self-center' to={`/profiles/${id}`}>
-					<Avatar src={image} height={imageSize} />
-				</Link>
-				<div className={`mx-2 ${styles.WordBreak}`}>
-					<strong>{owner}</strong>
-				</div>
-			</div> 
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+      </div>
+      <div className={`mx-2 ${styles.WordBreak}`}>
+        <strong>{owner}</strong>
+      </div>
+			
+			{/* Conditional display of follow or unfollow button */}
+      <div className={`text-right ${!mobile && "ml-auto"}`}>
+        {!mobile &&
+          currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+              onClick={() => {}}
+            >
+              unfollow
+            </Button>
+          ) : (
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Black}`}
+              onClick={() => {}}
+            >
+              follow
+            </Button>
+          ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
