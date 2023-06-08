97351e7c37dbc4634ce9b8afebb0cee78c77ea31
NickWaldock <nickwaldock@me.com>
2023-06-08 13:55:55 +0000

    Render profile data in ProfilePage.js, set props, variables and imports for the ProfileContextData

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -12,31 +12,73 @@ import btnStyles from "../../styles/Button.module.css";
 
 import PopularProfiles from "./PopularProfiles";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useProfileData, useSetProfileData } from "../../contexts/ProfileDataContext";
+import { Image } from "react-bootstrap";
+
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
   const currentUser = useCurrentUser();
+  const {id} = useParams();
+	const setProfileData = useSetProfileData();
+	const {pageProfile} = useProfileData();
+	const [profile] = pageProfile.results;
 
+	// API request to fetch profile data
   useEffect(() => {
-      setHasLoaded(true);
-  }, [])
+    const fetchData = async () => {
+        try{
+          const [{data: pageProfile}] = await Promise.all([
+						axiosReq.get(`/profiles/${id}/`)
+					])
+					setProfileData(prevState => ({
+						...prevState,
+						pageProfile: {results: [pageProfile]}
+					}))
+					setHasLoaded(true);
+        } catch(err){
+					console.log(err)
+        }
+    }
+    fetchData();
+  }, [id, setProfileData]);
 
   const mainProfile = (
     <>
       <Row noGutters className="px-3 text-center">
         <Col lg={3} className="text-lg-left">
-          <p>Image</p>
+          <Image className={styles.ProfileImage}
+						roundedCircle src={profile?.image}
+					/>
         </Col>
         <Col lg={6}>
-          <h3 className="m-2">Profile username</h3>
-          <p>Profile stats</p>
-        </Col>
-        <Col lg={3} className="text-lg-right">
-        <p>Follow button</p>
-        </Col>
-        <Col className="p-3">Profile content</Col>
-      </Row>
-    </>
+          <h3 className="m-2">{profile?.owner}</h3>
+          <Row className="justify-content-center no-gutters">
+						<Col xs={3} className="my-2">
+							<div>{profile?.posts_count}</div>
+							<div>posts</div>
+						</Col>
+
+						<Col xs={3} className="my-2">
+							<div>{profile?.followers_count}</div>
+							<div>followers</div>
+						</Col>
+
+						<Col xs={3} className="my-2">
+							<div>{profile?.following_count}</div>
+							<div>following</div>
+						</Col>
+
+						<Col lg={3} className="text-lg-right">
+							<p>Follow button</p>
+						</Col>
+						<Col className="p-3">Profile content</Col>
+					</Row>
+				</Col>
+			</Row>
+		</>
   );
 
   const mainProfilePosts = (
