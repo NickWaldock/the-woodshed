0391d9f5a166eac2937fd705cfc19f06291381a1
NickWaldock <nickwaldock@me.com>
2023-06-12 10:45:03 +0000

    Add useRedirect hook in PostCreateForm.js to redirect a non-autenticated user to the homepage

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -16,8 +16,13 @@ import Asset from "../../components/Asset";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 import { Alert } from "react-bootstrap";
+import { useRedirect } from "../../hooks/useRedirect";
+
 
 function PostCreateForm() {
+  // Redirect non-authenticated users to sign in page
+  useRedirect('loggedOut');
+
   const [errors, setErrors] = useState({});
 
   const [postData, setPostData] = useState({
