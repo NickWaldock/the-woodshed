04958be10a74107c0304712c15a7bbbfe4e371fa
NickWaldock <nickwaldock@me.com>
2023-06-12 11:06:47 +0000

    Create mocks folder and handlers.js file in /src and create mock api handlers for testing

---
+++ src/mocks/handlers.js
@@ -0,0 +1,20 @@
+import { rest } from "msw";
+
+const baseURL = 'https://the-woodshed-api.herokuapp.com/';
+
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+		return res(ctx.json({
+			"pk": 7,
+			"username": "DerrickLovesDrums",
+			"email": "",
+			"first_name": "",
+			"last_name": "",
+			"profile_id": 7,
+			"profile_image": "https://res.cloudinary.com/dnqpruigq/image/upload/v1/woodshed_media/../woodshed_media/profile-default_iqrrx0.png"
+			}));
+	}),
+	rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+		return res(ctx.status(200));
+	}),
+  ];
