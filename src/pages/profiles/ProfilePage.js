d106e5fc3a7df5239cedfb9dcdf5e47dbbe9070b
NickWaldock <nickwaldock@me.com>
2023-06-16 16:35:13 +0000

    Add font to Asset.module.css, add additional css classes in ProfilePage.moudle.css for fonts for the profile page

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -69,19 +69,19 @@ function ProfilePage() {
           />
         </Col>
         <Col lg={6}>
-          <h3 className="m-2">{profile?.owner}</h3>
+          <h3 className={`${styles.Title} m-2`}>{profile?.owner}</h3>
           <Row className="justify-content-center no-gutters">
-            <Col xs={3} className="my-2">
+            <Col xs={3} className={`${styles.Counts} my-2`}>
               <div>{profile?.posts_count}</div>
-              <div>posts</div>
+              <div>Posts</div>
             </Col>
-            <Col xs={3} className="my-2">
+            <Col xs={3} className={`${styles.Counts} my-2`}>
               <div>{profile?.followers_count}</div>
-              <div>followers</div>
+              <div>Followers</div>
             </Col>
-            <Col xs={3} className="my-2">
+            <Col xs={3} className={`${styles.Counts} my-2 ml-2`}>
               <div>{profile?.following_count}</div>
-              <div>following</div>
+              <div>Following</div>
             </Col>
           </Row>
         </Col>
@@ -112,7 +112,7 @@ function ProfilePage() {
   const mainProfilePosts = (
     <>
       <hr />
-      <p className="text-center">{profile?.owner}'s posts</p>
+      <p className={`${styles.Body} text-center`}>{profile?.owner}'s posts</p>
       <hr />
       {profilePosts.results.length ? (
         <InfiniteScroll
