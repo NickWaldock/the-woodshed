e65b7d9082a4df7ce1673795e57c51c7d4285e17
NickWaldock <nickwaldock@me.com>
2023-06-17 11:12:51 +0000

    Imported PopularProfiles component into PostPage.js for mobile and desktop

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -14,6 +14,7 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Asset from "../../components/Asset";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 
 function PostPage() {
@@ -44,7 +45,7 @@ function PostPage() {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
+        <PopularProfiles mobile />
         <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
         {currentUser ? (
@@ -81,7 +82,7 @@ function PostPage() {
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
+        <PopularProfiles />
       </Col>
     </Row>
   );
