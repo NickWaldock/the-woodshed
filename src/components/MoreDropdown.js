9e167f56fa80331ccf62671da9d756f35bf3137b
NickWaldock <nickwaldock@me.com>
2023-06-23 16:24:56 +0000

    Readjust and fix styling and class names for MoreDropdown.js and MoredropDown.module.css

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -14,65 +14,67 @@ const Gear = React.forwardRef(({ onClick }, ref) => (
       onClick(e);
     }}
   />
-  
 ));
 
 // Render the dropdown menu for editing and deleting a post
 export const MoreDropdown = ({ handleEdit, handleDelete }) => {
   return (
-    <Dropdown className="text-center" drop="right">
+    <Dropdown className={`ml-auto px-3 ${styles.Absolute} ${styles.Font} `} drop="right">
       <Dropdown.Toggle as={Gear} />
       <Dropdown.Menu
-        className={`${styles.General} ${styles.Container} text-center`}
+        className="text-center"
         popperConfig={{ strategy: "fixed" }}
+        className={styles.General}
       >
         <Dropdown.Item
-          className={styles.DropdownItem}
           onClick={handleEdit}
-          aria-label="edit"
+          aria-label="Edit Post"
+          className={styles.Hover}
         >
-          <div className={styles.IconBorder}>
-            <i className="fa-solid fa-pencil fa-lg"></i>
-          </div>
-          
+          Edit Post
+          <i className="fa-solid fa-pencil fa-lg ml-4" />
         </Dropdown.Item>
-        <Dropdown.Item
-          className={styles.DropdownItem}
+        <Dropdown.Item          
           onClick={handleDelete}
-          aria-label="delete"
+          aria-label="Delete"
+          className={styles.Hover}
         >
-          <div className={styles.IconBorder}>
-            <i className="fa-solid fa-trash-can fa-lg"></i>
-          </div>
+            Delete Post
+            <i className="fa-solid fa-trash-can fa-lg ml-2" />
         </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
 };
 
-
 export function ProfileEditDropdown({ id }) {
   const history = useHistory();
   return (
-    <Dropdown className={`ml-auto px-3 ${styles.Absolute} ${styles.Font}`} drop="left">
+    <Dropdown
+      className={`ml-auto px-3 ${styles.Absolute} ${styles.Font} `}
+      drop="left"
+    >
       <Dropdown.Toggle as={Gear} />
-      <Dropdown.Menu>
+      <Dropdown.Menu className={styles.General}>
         <Dropdown.Item
           onClick={() => history.push(`/profiles/${id}/edit`)}
-          aria-label="edit-profile"
+          aria-label="Edit Profile"
+          className={styles.Hover}
         >
           <i className="fa-solid fa-pencil mr-2" /> Edit Profile
         </Dropdown.Item>
         <Dropdown.Item
           onClick={() => history.push(`/profiles/${id}/edit/username`)}
-          aria-label="edit-username"
+          aria-label="Edit Username"
+          className={styles.Hover}
         >
           <i className="fa-solid fa-signature mr-2" />
           Change Username
         </Dropdown.Item>
         <Dropdown.Item
           onClick={() => history.push(`/profiles/${id}/edit/password`)}
-          aria-label="edit-password"
+          aria-label="Edit Password"
+          className={styles.Hover}
         >
           <i className="fa-solid fa-lock mr-2" />
           Change Password
